ec691c6ee93457a9a13266c3eefcd15053be1da5
theddy9098 <teddyfso15@gmail.com>
2025-09-30 21:14:50 -0500

    Implementacion de la Conexion a la Base de datos

--- src/main/java/CEA_Indiv/Infrastructure/ConfiBD/ConexionBD.java
+++ src/main/java/CEA_Indiv/Infrastructure/ConfiBD/ConexionBD.java
@@ -1,4 +1,22 @@
 package CEA_Indiv.Infrastructure.ConfiBD;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class ConexionBD {
+    private static final String URL = "jdbc:postgresql://localhost:5432/actividad";
+    private static final String USUARIO = "Teddy";
+    private static final String CONTRASENA = "9098";
+
+    public static Connection getConnection() {
+        Connection connection = null;
+        try {
+            Class.forName("org.postgresql.Driver");
+            connection = DriverManager.getConnection(URL, USUARIO, CONTRASENA);
+        } catch (ClassNotFoundException | SQLException e) {
+            System.err.println("Error al conectar a la base de datos: " + e.getMessage());
+        }
+        return connection;
+    }
 }
